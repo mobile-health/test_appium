7753f241e96462ba74395fec4d4e8a8b599f5f1b
miapham <mia.pham@manadr.com>
2022-09-13 11:00:59 +0700

    merge

--- src/testcase/__init__.py
+++ src/testcase/__init__.py
@@ -1,10 +1,7 @@
-<<<<<<< HEAD
 from os import uname
 # from .register import RegisterSuccessful
 # from .login import LoginSuccessful
 # from .appointment import BookApptSuccessful
 from .register import testRegisterByPhone
 from .teleconsult import ProviderPool
-=======
 from .teleconsult import ProviderPool
->>>>>>> 26654941ccae6936f2ac07e36f2d8022b7ad2588
